615d7d5f17f621f994cde8b46d0fa826c4d2b5d1
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2023-01-26 20:24:24 +0300

    1.4 start message ready

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
+    private final String ERROR_TEXT = "Извини, я ограничен в ответах, напиши /start - для начала работы";
+    private final String START_TEXT = "Приветствую, я бот который поможет не забыть";
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
@@ -25,10 +29,16 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     }
 
     @Override
-    public int process(List<Update> updates) {
+    public int process(List<Update> updates) { // обработка запросов бота
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if ((update.message().text()).equals("/start")) {
+                SendMessage startMessage = new SendMessage(update.message().chat().id(), START_TEXT);
+                SendResponse startResponse = telegramBot.execute(startMessage);
+            } else {
+                SendMessage errorMessage = new SendMessage(update.message().chat().id(), ERROR_TEXT);
+                SendResponse errorResponse = telegramBot.execute(errorMessage);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
